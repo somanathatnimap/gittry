2c58d641d66b0621ffd4522f336d429369cb797f
Somanath Barge <somanath@nimap.com>
2023-06-16 16:11:35 +0530

    second commit

---
+++ Daily Codes From 14 June/Interface_Example.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Codes_From_14_June
+{
+    internal class Class3
+    {
+        interface Employee
+        {
+            void EmployeeDetails();
+        }
+        interface Person
+        {
+            void EmployeeDetails();
+        }
+        class Manager : Employee, Person  
+        {
+              void Employee.EmployeeDetails()
+            {
+                Console.WriteLine("This is interface..!");                
+            }
+             void Person.EmployeeDetails() {
+                Console.WriteLine("This is also interface..!");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Manager m= new Manager();
+            ((Employee)m).EmployeeDetails();
+                Console.ReadLine();
+
+            int i = 0;
+            while (i<100)
+            {
+              Console.WriteLine(i);
+                i++;
+            }
+            
+        }
+    }
+}
