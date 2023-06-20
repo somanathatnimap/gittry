c02bc296ea160c762974d9e37c500028e95ff7ec
Somanath Barge <somanath@nimap.com>
2023-06-20 12:45:34 +0530

    first commit

---
+++ PartialExp/PartialExp/PartialExample1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialExp
+{
+    internal partial class PartialExample
+    {
+        public  int sub(int a, int b)
+        {
+            Console.WriteLine(a + b);
+            return a + b;
+        }
+    }
+}
