62bf99b7edc92aba478573ff118bc28d2b56b507
Somanath Barge <somanath@nimap.com>
2023-06-22 18:15:56 +0530

    thursday

---
+++ Daily Codes From 14 June/HashClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Codes_From_14_June
+{
+    internal class HashClass
+    {
+        public static void Main(string[] args)
+        {
+            Hashtable hashtable = new Hashtable()
+            {
+                { "Id",1},{"Name","Somanath"},{"Sal",10000},{"Dept","Btech"}
+            };
+            foreach(object key in  hashtable.Keys)
+            {
+                Console.WriteLine(key+" :"+hashtable[key]);
+            }
+            Console.WriteLine("--------------------");
+            Console.WriteLine(hashtable.Contains("Id"));
+            Console.WriteLine(hashtable.GetHashCode());
+            hashtable.Remove("Id");
+
+            foreach(object key in hashtable.Keys)
+            {
+                Console.WriteLine(key+" " + hashtable[key]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
