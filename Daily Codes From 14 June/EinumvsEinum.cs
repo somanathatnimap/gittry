246e1fc2eb929ab6c42ff043d1dfac8734c278d8
Somanath Barge <somanath@nimap.com>
2023-06-27 17:55:41 +0530

    tuesday

---
+++ Daily Codes From 14 June/EinumvsEinum.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Codes_From_14_June
+{
+    internal class EinumvsEinum
+    {
+        class IEnumerablevsIEnumerator
+        {
+            static void Main(string[] args)
+            {
+                List<int> list = new List<int>();
+                list.Add(1910);
+                list.Add(1920);
+                list.Add(1930);
+                list.Add(1940);
+                list.Add(1950);
+                list.Add(1960);
+                //this is ienumerable
+                IEnumerable<int> values=(IEnumerable<int>)list;
+                foreach(int value in values)
+                {
+                    Console.WriteLine(value);
+                }
+                //this is ienumerator
+                IEnumerator<int> enumerator = list.GetEnumerator();
+                while (enumerator.MoveNext())
+                {
+                    Console.WriteLine(enumerator.Current.ToString());
+                }
+
+                //boxing and unboxing
+                int num = 10;
+                object obj = num;//implicit conversion of value type into reference type
+                
+                object obj2 = 20;
+                int val=(int)obj;//unboxing
+
+                Console.ReadLine();
+
+            }
+           
+
+        }
+    }
+}
---
+++ Daily Codes From 14 June/OutRef.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Codes_From_14_June
+{
+    internal class OutRef
+    {
+        class Hello
+        {
+            static int Add(int x)
+            { //pass by value
+                x = x + 10;
+                Console.WriteLine("pass by value"+x);
+                return x;
+            }
+            static int Add1(ref int y)
+            {
+                y = y + 10;
+                Console.WriteLine("pass by refrance "+y);
+                return y;
+            }
+            //now out
+            static void Add3(out  int z)
+            {
+                 z = 10;
+                Console.WriteLine("pass by out"+z);
+            }
+
+            static void Main(string[] args)
+            {   //pass by value
+                //Hello hello = new Hello();
+                int value = 5;
+                Add(value);
+                Console.WriteLine(value);
+
+                int value1 = 5;
+                Add1(ref value1);
+                Console.WriteLine(value1);
+
+                //pass by out
+                int value2;
+                Add3(out value2);
+                Console.WriteLine(value2);
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
