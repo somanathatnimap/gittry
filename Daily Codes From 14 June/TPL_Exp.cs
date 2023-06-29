955422d488c938bce91afd1973ecf482b3b9cde0
Somanath Barge <somanath@nimap.com>
2023-06-29 19:06:31 +0530

    thr

---
+++ Daily Codes From 14 June/TPL_Exp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Daily_Codes_From_14_June
+{
+    internal class TPL_Exp
+    {
+        class program
+        {
+            static void LoopEx()
+            {
+                for(int i = 0; i < 1000; i++)
+                {
+                    //string x = "";
+                    Console.WriteLine(i);
+                }
+
+            Console.ReadLine();
+            }
+            static void Main()
+            {
+                Parallel.For(0, 10, x => LoopEx());
+                
+            }
+        }
+    }
+}
+
