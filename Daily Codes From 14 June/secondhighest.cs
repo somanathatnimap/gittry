3970e28f9e0303b9a2d05b95085acb8fe5b314ab
Somanath Barge <somanath@nimap.com>
2023-07-03 19:14:34 +0530

    mon

---
+++ Daily Codes From 14 June/secondhighest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Codes_From_14_June
+{
+    internal class secondhighest
+    {
+        class secondhight
+        {
+            static void Main(string[] args)
+            {
+                int[] arr = { 12, 35, 1, 10, 34, 1, 35 };
+                int largest = 0;
+                int secondlargest = 0;
+                foreach (int i in arr)
+                {
+                    if (i > largest)
+                    {
+                        secondlargest = largest;
+                        largest = i;
+                    }
+                    else if(largest > i && secondlargest<i)
+                    {
+                        secondlargest = i;
+                    }
+                }
+                    Console.WriteLine(secondlargest);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Daily Codes From 14 June/varvsdnyamic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Codes_From_14_June
+{
+    internal class varvsdnyamic
+    {
+        public class May
+        {
+            public int maybe()
+            {
+                var a = 10;
+                a = 101;
+                return a;
+            }
+            static void Main(string[] args)
+            {
+                object ab = 1000;
+                var a = "20";
+                dynamic b = 20;
+                b = "soma";
+                Console.WriteLine(b is string);
+                Console.WriteLine(a as string);
+                Console.WriteLine(ab);
+                Console.WriteLine(a);
+                Console.ReadLine();
+
+            }
+        }
+        }
+    }
+
