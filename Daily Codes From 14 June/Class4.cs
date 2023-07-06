60783d94f69b2ea69c163a995afc9ba1a575f288
Somanath Barge <somanath@nimap.com>
2023-07-06 18:46:53 +0530

    thursday

---
+++ Daily Codes From 14 June/Class4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Codes_From_14_June
+{
+    /* class program
+     {
+         int a,n;
+         public void show()
+         {
+             a=0 ;
+             Console.WriteLine(" n:"+n);
+         }
+     }
+     {
+         static void Main()
+         {
+             program p=new program();
+             p.show();
+
+             Console.ReadLine();
+         }
+     }
+ }
+    */
+    internal class Class4
+    {
+        class program
+        {
+
+        }
+    }
+}
