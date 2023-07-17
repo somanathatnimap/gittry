35939122f60e6f83c253823c75fb8ba11c3d4c6a
Somanath Barge <somanath@nimap.com>
2023-07-17 18:47:43 +0530

    monday

---
+++ Daily Codes From 14 June/FileSystem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.MemoryMappedFiles;
+
+namespace Daily_Codes_From_14_June
+{
+    internal class FileSystem
+    {
+
+class Test
+    {
+        public static void Main()
+        {
+            string path = @"C:\Users\Nimap\Desktop\MyText.txt";
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Hi,");
+                    sw.WriteLine("I'm");
+                    sw.WriteLine("Somanath Barge");
+                }
+            }
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
+}
---
+++ Daily Codes From 14 June/TryCatch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Codes_From_14_June
+{
+    internal class TryCatch
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                int a = 10;
+                int b = 0;
+                Console.WriteLine(a / b);
+            }
+            catch (DivideByZeroException)
+            { }
+
+            Console.ReadLine();
+
+        }
+    }
+}
+ 
