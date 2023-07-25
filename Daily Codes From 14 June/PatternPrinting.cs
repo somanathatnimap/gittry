c528ab1e8d757c966548db74de8612e97c91701e
Somanath Barge <somanath@nimap.com>
2023-07-25 17:17:56 +0530

    tuesday.

---
+++ Daily Codes From 14 June/PatternPrinting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Codes_From_14_June
+{
+    internal class PatternPrinting
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0;i<5;i++)
+            {
+                for(int j = 5; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }Console.ReadLine();
+        }
+    }
+}
