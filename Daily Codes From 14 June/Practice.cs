ccf7d2ba0d8eb82bbce4a653f3bfd91ad8097280
Somanath Barge <somanath@nimap.com>
2023-07-31 18:14:27 +0530

    31/07/23

---
+++ Daily Codes From 14 June/Practice.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Codes_From_14_June
+{
+    internal class Practice
+    {
+        class Profile
+        {
+            protected int id;
+           public void Set(int id)
+            {
+                this.id = id;
+            }
+            public void Get()
+            {
+                Console.WriteLine("id :"+id);
+            }
+        }
+        class Pr : Profile
+        {
+            public string name;
+            public void Set(string name)
+            {
+                this.name=name;
+            }
+            public void Gett()
+            {
+                Console.WriteLine("name :"+name);
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            
+            Pr p=new Pr();  
+            p.Set(1);
+            p.Get();
+            Console.ReadLine();
+        }
+    }
+}
