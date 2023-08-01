1eb89639863737dcbb3becf85875ad2f1c21cd6b
Somanath Barge <somanath@nimap.com>
2023-08-01 19:19:38 +0530

    1/8/23

---
+++ Daily Codes From 14 June/AllOOPs.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Codes_From_14_June
+{
+    
+class Vehicle
+    {
+        public string Brand { get; set; }
+        public int Year { get; set; }
+
+        public Vehicle(string brand, int year)
+        {
+            Brand = brand;
+            Year = year;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            //interpolation
+            Console.WriteLine($"Brand: {Brand}, Year: {Year}");
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public int NumDoors { get; set; }
+        public Car(string brand, int year, int numDoors) : base(brand, year)
+        {
+            NumDoors = numDoors;
+        }
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Car - Brand: {Brand}, Year: {Year}, Number of Doors: {NumDoors}");
+        }
+    }
+    class Motorcycle : Vehicle
+    {
+        public bool HasSidecar { get; set; }
+
+        public Motorcycle(string brand, int year, bool hasSidecar) : base(brand, year)
+        {
+            HasSidecar = hasSidecar;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Motorcycle - Brand: {Brand}, Year: {Year}, Has Sidecar: {HasSidecar}");
+        }
+    }
+    /*
+    class Program
+    {
+        static void Main()
+        {
+            // Using polymorphism to store different types of vehicles in the same array
+            Vehicle[] vehicles = new Vehicle[]
+            {
+            new Car("Toyota", 2020, 4),
+            new Motorcycle("Harley Davidson", 2019, false),
+            new Car("Honda", 2022, 2)
+            };
+
+            // Displaying information about each vehicle
+            foreach (Vehicle vehicle in vehicles)
+            {
+                vehicle.DisplayInfo();
+            }
+    }   
+    }
+    }
+    */
+
+}
+
--- Daily Codes From 14 June/All_Concepts.cs
+++ Daily Codes From 14 June/All_Concepts.cs
@@ -3,113 +3,132 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Daily_Codes_From_14_June
 {
     internal class All_Concepts
+
+    {
+    /*
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Main Method Started......");
+
+                SomeMethod();
+
+                Console.WriteLine("Main Method End");
+                Console.ReadKey();
+            }
+
+            public async static void SomeMethod()
+            {
+                Console.WriteLine("Some Method Started......");
+
+               await Task.Delay(TimeSpan.FromSeconds(10));
+                Console.WriteLine("\n");
+                Console.WriteLine("Some Method End");
+            }
+        }
+    
+    
+    public static void Main(string[] args)
     {
 
+    string s1 = "soma";
+    char[] chars = s1.ToCharArray();
+    Array.Reverse(chars);
+        Console.WriteLine(chars);
+        Console.ReadLine();
+    }
+    */
 
+    /*
+    static void Main(string [] args)
+    {
+    int a;
+        Func<int,int, int,int> Add=(x,y,z)=>x+y+z;
+        Console.WriteLine(Add(1,2,3));
 
-
-        /*
-        public static void Main(string[] args)
+        IEnumerable<int> list = new List<int>() { 1,2,3,4};
+        foreach (int i in list)
         {
-
-        string s1 = "soma";
-        char[] chars = s1.ToCharArray();
-        Array.Reverse(chars);
-            Console.WriteLine(chars);
-            Console.ReadLine();
+            Console.WriteLine(i);
         }
+      void  add(out a);
+
+        Console.WriteLine(a);
+
+
+
+        Console.ReadLine();
+
+    }
+   public void add(out a)
+    {
+        a = 10;
+    }
         */
 
-        /*
-        static void Main(string [] args)
-        {
-        int a;
-            Func<int,int, int,int> Add=(x,y,z)=>x+y+z;
-            Console.WriteLine(Add(1,2,3));
+    /*
 
-            IEnumerable<int> list = new List<int>() { 1,2,3,4};
-            foreach (int i in list)
-            {
-                Console.WriteLine(i);
-            }
-          void  add(out a);
+     int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            Console.WriteLine(a);
-            
-            
-
-            Console.ReadLine();
-
-        }
-       public void add(out a)
-        {
-            a = 10;
-        }
-            */
-
-        /*
-         
-         int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-        // Create a Task to process the numbers in parallel
-        Task[] tasks = new Task[numbers.Length];
-        for (int i = 0; i < numbers.Length; i++)
-        {
-            int num = numbers[i];
-            tasks[i] = Task.Run(() => ProcessNumber(num));
-        }
-
-        // Wait for all tasks to complete
-        Task.WaitAll(tasks);
-
-        Console.WriteLine("All tasks are completed.");
-    }
-
-    static void ProcessNumber(int num)
+    // Create a Task to process the numbers in parallel
+    Task[] tasks = new Task[numbers.Length];
+    for (int i = 0; i < numbers.Length; i++)
     {
-        // Simulate some processing time
-        Thread.Sleep(1000);
-        Console.WriteLine($"Processed number: {num}, Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+        int num = numbers[i];
+        tasks[i] = Task.Run(() => ProcessNumber(num));
     }
+
+    // Wait for all tasks to complete
+    Task.WaitAll(tasks);
+
+    Console.WriteLine("All tasks are completed.");
+}
+
+static void ProcessNumber(int num)
+{
+    // Simulate some processing time
+    Thread.Sleep(1000);
+    Console.WriteLine($"Processed number: {num}, Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+}
 }
 
 
-         */
+     */
 
-        /*
-        class Program
+    /*
+    class Program
+    {
+        static async Task Main()
         {
-            static async Task Main()
-            {
-                int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-                var tasks = ProcessNumbersAsync(numbers);
+            var tasks = ProcessNumbersAsync(numbers);
 
-                await Task.WhenAll(tasks);
+            await Task.WhenAll(tasks);
 
-                Console.WriteLine("All tasks are completed.");
-            }
-
-            static async Task ProcessNumbersAsync(int[] numbers)
-            {
-                foreach (int num in numbers)
-                {
-                    await ProcessNumberAsync(num);
-                }
-            }
-
-            static async Task ProcessNumberAsync(int num)
-            {
-                await Task.Delay(1000);
-                Console.WriteLine($"Processed number: {num}, Thread ID: {Environment.CurrentManagedThreadId}");
-            }
-     
+            Console.WriteLine("All tasks are completed.");
         }
-        */
+
+        static async Task ProcessNumbersAsync(int[] numbers)
+        {
+            foreach (int num in numbers)
+            {
+                await ProcessNumberAsync(num);
+            }
+        }
+
+        static async Task ProcessNumberAsync(int num)
+        {
+            await Task.Delay(1000);
+            Console.WriteLine($"Processed number: {num}, Thread ID: {Environment.CurrentManagedThreadId}");
+        }
+
     }
+    */
 }
+
