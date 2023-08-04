af71da40a075d7cfd7bf043bb060969e0197535b
Somanath Barge <somanath@nimap.com>
2023-08-04 20:07:43 +0530

    4/8/23

---
+++ Daily Codes From 14 June/ReverseNo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Codes_From_14_June
+{
+    internal class ReverseNo
+    {
+        static void Main(string[] args)
+        {
+        int a= 123;
+        int temp = 0;
+        int rev = 0;
+         
+            while(a>0)
+            {
+                temp = a % 10;
+                rev=(rev*10)+temp;
+                a=a/10;
+            }
+                Console.WriteLine(rev);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Daily Codes From 14 June/sechigh.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Codes_From_14_June
+{
+    internal class sechigh
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 4, 5, 2, 6, 1, 9, 5 };
+            int high = 0;
+            int sechigh = 0;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > high)
+                {
+                    sechigh = high;
+                    high = arr[i];
+                }
+                else if (arr[i] > sechigh && arr[i] < high)
+                {
+                    sechigh = arr[i];
+                }
+            }
+                Console.WriteLine(sechigh);
+                Console.ReadKey();
+            
+        }
+    }
+}
