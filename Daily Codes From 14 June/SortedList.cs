9b1a45485025ba0d96b701d0b4dda89f4d55e75c
Somanath Barge <somanath@nimap.com>
2023-08-09 19:28:31 +0530

    9/8/2023

---
+++ Daily Codes From 14 June/SortedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Codes_From_14_June
+{
+    internal class SortedList
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 4 };
+            List<int> list1 = new List<int>() { 1, 3, 4 };
+            List<int> sortedlist = new List<int>();
+
+
+        }
+    }
+}
+
+/*
+ 
+ List<int> list = new List<int>() { 1, 2, 4 };
+            List<int> list1 = new List<int>() { 1, 3, 4 };
+            List<int> sortedlist = new List<int>();
+            for (int i=0;i<list.Count;i++)
+            {
+                sortedlist.Add(list[i]);
+               // Console.WriteLine("added list" + list[i]);
+            }
+            for(int i=0; i < list1.Count; i++)
+            {
+                sortedlist.Add(list1[i]);
+            }
+           // sortedlist.Sort();
+            for(int i=0;i< sortedlist.Count; i++)
+            {
+                Console.WriteLine(sortedlist[i]);
+            }
+            Console.ReadLine();
+ 
+ */
